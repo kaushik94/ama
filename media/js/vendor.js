ea262632e7161f3c97c44b32bfdf1fd24d8332ee
Anurag Agarwal <anurag.agarwal561994@gmail.com>
2015-10-10 10:35:48 +0530

    Adds vendor package quill for text editor

--- media/js/vendor.js
+++ media/js/vendor.js
@@ -16,4 +16,5 @@
 //= require vendor/at/jquery.caret.js
 //= require vendor/at/jquery.atwho.js
 //= require vendor/backbone.keys/backbone.keys.js
+//= require vendor/quill/quill.min.js
 //= require common.js
